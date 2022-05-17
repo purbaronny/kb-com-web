1ec3e468e1febe9027573bd539d0e58db2faeb68
hafizhx12345 <hafizh.haritsa@kbds5i.com>
2022-05-17 11:21:59 +0700

    adding img-crop

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,9 +4,7 @@ import { Link } from "react-router-dom";
 import DetailCareer from "./DetailCareer";
 import "./Card.css";
 
-
 class Card extends Component {
-
   constructor(props) {
     super(props);
     let dateString = props.text1;
@@ -14,14 +12,13 @@ class Card extends Component {
 
     this.state = {
       date: dateResult.toString(),
-
-    }
+    };
   }
 
   changeLanguage = (val) => {
     this.props.changeLanguage(val);
   };
-  componentWillUnmount() { }
+  componentWillUnmount() {}
 
   onLanguageClick = (value) => {
     this.setState(
@@ -38,10 +35,14 @@ class Card extends Component {
     return (
       <div className="col-md-4 col-10 mx-auto">
         <div className="card">
-          <img src={this.props.imgsrc} className="img-card" alt="..." />
+          <div className="img-card">
+            <img src={this.props.imgsrc} className="img-inside" alt="..." />
+          </div>
           <div className="card-body ">
             <h5 className="card-title card-title-h5">{this.props.title}</h5>
-            <p className="card-text card-topic-p">{this.props.expired} : {this.state.date}</p>
+            <p className="card-text card-topic-p">
+              {this.props.expired} : {this.state.date}
+            </p>
             <Link to={`/career/${this.props.code}`} className="btn orange-color" key={this.props.code}>
               {this.props.linkSourceText}
             </Link>
